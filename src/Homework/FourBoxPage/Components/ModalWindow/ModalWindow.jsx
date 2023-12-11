677ade66d7a1ef88d4b6cd4206edabf5d5b85462
GevSarg <=>
2023-12-10 22:56:39 -0800

    User Page v2

--- src/Homework/FourBoxPage/Components/ModalWindow/ModalWindow.jsx
+++ src/Homework/FourBoxPage/Components/ModalWindow/ModalWindow.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import "./ModalWindow.scss";
 import PropTypes from "prop-types";
 
 export default function ModalWindow({ toggleModal, handleDelete }) {
+  useEffect(() => {
+    const handleClick = (e) => {
+      const { className } = e.target;
+      if (className === "Modal") {
+        toggleModal();
+      }
+    };
+    window.addEventListener("click", handleClick);
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
   return (
     <div className="Modal">
       <div className="Modal__Content">
