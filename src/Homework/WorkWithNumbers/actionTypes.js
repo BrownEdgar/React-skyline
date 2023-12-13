20e180fd104cad5a007b7150db316ae65846ba96
GevSarg <=>
2023-12-12 22:50:29 -0800

    WOrk With Numbers

---
+++ src/Homework/WorkWithNumbers/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_RANDOM_NUMBERS = "add-random-numbers";
+export const ADD_ONE_NUMBER = "add-one-number";
+export const REMOVE_ONE_NUMBER = "remove-one-number";
+export const SORT_ORDER = "sort";
+export const CLEAR = "clear";
