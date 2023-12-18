725b5f7fac7d15001c84dc1ce2d81675a19c0ee2
Artyom <artomdavtan1@gmail.com>
2023-12-18 18:13:43 +0300

    111

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,94 @@
-import React from "react";
-import { useReducer } from "react";
-import reducer from "./reducer";
-import { FaAlignJustify } from "react-icons/fa6";
-import { CLEAR, SORT_NUMS, RANDOM_NUMBER, NUMBERS } from "./actionTypes";
-import "./App.scss";
+import { useState } from 'react'
+import'./App.scss'
+import { nanoid } from "nanoid";
+import { FaRegEye } from "react-icons/fa6";
+import { GiErlenmeyer } from "react-icons/gi";
+import { FaRegEyeSlash } from "react-icons/fa6";
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, [3, 2, 1]);
-
-  const handleSort = () => {
-    dispatch({ type: SORT_NUMS });
-  };
-
-  const handleClear = () => {
-    dispatch({ type: CLEAR });
-  };
-
-
-  const handleClick = () => {
-    dispatch({type: RANDOM_NUMBER, payload: Math.round(Math.random() * 100)})
+  const [currentId, setcurrentId] = useState(null);
+  const [users, setUsers] = useState([{
+    id: 1,
+    username: "sebstian",
+    email: 'seb122.gmail.com',
+    password: 'dasdasdas'
+  }])
+ 
+const saveCurrentId = (userid) => setcurrentId(userid)
+ 
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { username, email } = e.target;
+    const isUserExist = users.some((user) => user.name === username.value.trim())
+    const newUser = {
+      id: nanoid(),
+      username: username.value.trim(),
+      email: email.value.trim(),
+      password: password.value
+    };
+    setUsers([...users, newUser])
+    e.target.reset()
   }
 
-  const addNums = () => {
-    dispatch({type: NUMBERS})
-  }
+
 
 
 
   return (
-    <div>
-      <div className="a">
-        <div className="btnscontainer">
-          <button onClick={addNums}>Add 10 </button>
-          <button onClick={handleClick}>Add Number</button>
-          <button onClick={handleSort}>Sort numbers</button>
-          <button onClick={handleClear}>Clear all</button>
-        </div>
+    <>
+      <div className="App">
+        <form id="myForm" onSubmit={handleSubmit}>
+          <label htmlFor="username" className="userns" required>
+            Username
+          </label>
+          <input form="myForm" type="text" id="username" required />
+          <label htmlFor="email">Email</label>
+          <input form="myForm" type="email" id="email" required />
+          <label htmlFor="password">Password</label>
+          <input form="myForm" type="password" id="password" required />
+
+          <span>
+            Create account? <GiErlenmeyer />
+          </span>
+          <input
+            form="myForm"
+            onSubmit={handleSubmit}
+            type="submit"
+            value="Submit"
+            className="button"
+          />
+        </form>
       </div>
-      <div className="box-container">
-        {state.map((el) => {
-          return (
-            <div className="box" key={el.id}>
-              {el}
-            </div>
-          );
-        })}
+      <div className="container">
+        <ul>
+          {users.map((user) => {
+            return (
+              <li key={user.id}>
+                <span>Name: {user.username}</span>
+                <span>Email: {user.email}</span>
+                <span>
+                  Password:{" "}
+                  {user.id === currentId ? (
+                    <>
+                      {user.password}
+                      <FaRegEyeSlash className='slashEye' onClick={() => setcurrentId(null)}/>
+                    </>
+                  ) : (
+                    <>
+                      {" "}
+                      {"*".repeat(10)}
+                      <button className="eye">
+                        <FaRegEye onClick={() => saveCurrentId(user.id)} />
+                      </button>
+                    </>
+                  )}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
+    </>
   );
 }
--- src/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const RANDOM_NUMBER = 'random-number'
-export const SORT_NUMS = 'sort-numbers'
-export const CLEAR = "clear-numbers";
-export const NUMBERS = 'add-numbers';
