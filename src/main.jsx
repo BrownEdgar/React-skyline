f9737818b10339028691a679c165e095b228329e
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-20 12:55:34 +0400

    added form example 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './Lessons/FORM/App'
+import App from './Lessons/FORM/select/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
